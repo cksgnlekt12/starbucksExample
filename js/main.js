9f702b21b067e1bb819945676172f8d7d5e7db95
cksgnlekt12 <cksgnlekt1@naver.com>
2021-10-17 22:26:47 +0900

    2021.10.17 공부 내용 적용

--- js/main.js
+++ js/main.js
@@ -16,4 +16,39 @@ searchInputEl.addEventListener('focus',function(){
 searchInputEl.addEventListener('blur',function(){
   searchEl.classList.remove("focused");
   searchInputEl.setAttribute('placeholder', '');
-})
+});
+
+
+
+const badgeEl = document.querySelector("header .badges");
+
+window.addEventListener('scroll',_.throttle(function(){
+  if (window.scrollY > 500) {
+    gsap.to(badgeEl, .3, {
+      opacity:0,
+      display:'none'
+    });
+  }else{
+    gsap.to(badgeEl, .3, {
+      opacity:1,
+      display:'block'
+    });
+  }
+},300));
+
+
+const fadeEls = document.querySelectorAll(".visual .fade-in");
+
+fadeEls.forEach(function(fadeEl, index){
+  gsap.to(fadeEl, 1, {
+    delay:index * 0.7,
+    opacity:1
+  });
+});
+
+
+new Swiper('.notice-line .swiper-container', {
+  direction: 'vertical',
+  autoplay: true,
+  loop: true
+});
